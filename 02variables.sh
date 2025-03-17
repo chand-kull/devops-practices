85d863459b79a7d3a44732a56d9bc2dc4860da03
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-17 23:30:58 +0530

    this is my first commit

--- 02variables.sh
+++ 02variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "chandu :: hey hai pavan how are you"
-echo "pavan  :: hi chandu, i am fine what about you"
-echo "chandu :: i am fine too, how is it your work"
-echo "pavan  :: hmm not bad ......"
+PERSON1=chandu
+PERSON2=pavan
+
+echo "$PERSON1 :: hey hai $PERSON2 how are you"
+echo "$PERSON2  :: hi $PERSON1, i am fine what about you"
+echo "$PERSON1 :: i am fine too, how is it your work"
+echo "$PERSON2  :: hmm not bad ......"
